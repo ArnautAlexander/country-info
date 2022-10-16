e01ef3683bcbb5d8d2b23e716e084e18ce4a1a20
arnautalexunder <alex86under@gmail.com>
2022-10-16 14:36:13 +0300

    preparing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <h1>Тестируем2</h1>
-  </React.StrictMode>
+  <ChakraProvider>
+    <Heading>Тестируем2</Heading>
+  </ChakraProvider>
 );
