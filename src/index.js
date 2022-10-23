6c1eff1f9f3e40a51f8ac451f443c32bee61f9cb
arnautalexunder <alex86under@gmail.com>
2022-10-23 17:37:02 +0300

    t3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Heading } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <h1>Тестируем2</h1>
-  </React.StrictMode>
+  <ChakraProvider>
+    <Heading>Тестируем2</Heading>
+  </ChakraProvider>
 );
